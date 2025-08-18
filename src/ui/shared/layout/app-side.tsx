87c09e654ec6e049a218432735a5fa6c5f209d2e
Luis Colunga Perez <perez.luis.colunga@gmail.com>
2025-08-17 23:33:42 -0600

    ✨  Feat: Add vialyzer logo

--- src/ui/shared/layout/app-side.tsx
+++ src/ui/shared/layout/app-side.tsx
@@ -1,4 +1,10 @@
-import { Sidebar, SidebarContent, SidebarHeader, SidebarRail } from '../sidebar'
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarHeader,
+  SidebarRail,
+  useSidebar,
+} from '../sidebar'
 import {
   Camera,
   FileChartColumnIncreasing,
@@ -10,10 +16,17 @@ import {
   User,
   Video,
 } from 'lucide-react'
+import { cn } from '@/utils'
+import { config } from '@/utils/config'
 import { NavSection } from './nav-section'
+
+import Logo from '@/assets/icons/logo.svg'
+
 import type { INavSection } from './types'
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { open } = useSidebar()
+
   const principalMenu: INavSection = {
     group: 'Principal',
     items: [
@@ -101,7 +114,17 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar collapsible='icon' {...props}>
       <SidebarHeader>
-        <div className='w-full grid place-content-center'>DS</div>
+        <a
+          href='https://sittycia.com/'
+          target='_blank'
+          rel='noreferrer'
+          className={cn('flex flex-row items-center gap-3', open && 'px-4')}>
+          <img src={Logo} className='size-8' alt='Vialyzer Dashboard Logo' />
+          <span className='sr-only'>{config.name}</span>
+          {open ? (
+            <span className='text-xl font-semibold'>{config.name}</span>
+          ) : null}
+        </a>
       </SidebarHeader>
       <SidebarContent>
         <NavSection group={principalMenu.group} items={principalMenu.items} />
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  name: 'dashboard-template',
+  name: 'Vialyzer',
   apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
 }
