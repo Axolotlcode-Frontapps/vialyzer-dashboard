83f603434967d6d39406dd4dd31c77f1b23197c2
Luis Colunga Perez <perez.luis.colunga@gmail.com>
2025-08-19 13:55:34 -0600

    ✨  Feat: Add search validator to roles

--- src/ui/roles/roles-table/index.tsx
+++ src/ui/roles/roles-table/index.tsx
@@ -4,7 +4,6 @@ import { settingsService } from '@/lib/services/settings'
 import { DataTable } from '@/ui/shared/data-table'
 import { DataTableHeader } from '@/ui/shared/data-table/table-header'
 import { columns } from './columns'
-import { RoleAdd } from '../role-add'
 import { TableSkeleton } from '@/ui/shared/data-table/table-skeleton'
 
 export function RolesTable() {
@@ -16,11 +15,7 @@ export function RolesTable() {
   const roles = useMemo(() => rolesData?.payload ?? [], [rolesData])
 
   return (
-    <div className='space-y-4'>
-      <div className='flex items-center justify-between gap-4'>
-        <h2 className='text-xl lg:text-2xl font-medium'>Roles</h2>
-        <RoleAdd />
-      </div>
+    <>
       {!isLoading ? (
         <DataTable columns={columns} data={roles}>
           {({ table }) => (
@@ -34,6 +29,6 @@ export function RolesTable() {
       ) : (
         <TableSkeleton />
       )}
-    </div>
+    </>
   )
 }
